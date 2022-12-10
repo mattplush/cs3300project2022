222db32c99c528b03f4192f3fa96affd8f27f3cd
mattplush <mplush2@uccs.com>
2022-12-10 01:01:36 +0000

    Iteration 06, 6:01pm, 12/9/22

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   #root "projects#index"
   root to: "home#index"
   resources :projects
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #root "articles#index"
 end
